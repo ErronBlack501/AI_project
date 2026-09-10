ad4dba28471b398b32b4cca7c6706ccb98c66e18
AndriFitia <andri.fitiavana126@gmail.com>
2026-09-10 16:08:28 +0300

    pdf_extractor.py : correction pour rpise en compte de plus de modèles de CV

--- app/extraction/pdf_extractor.py
+++ app/extraction/pdf_extractor.py
@@ -10,11 +10,21 @@ def extract_text_pdf(pdf_path: str) -> str:
     Returns:
         str: le texte extrait du PDF.
     """
+
     document = pymupdf.open(pdf_path)
     pages_text = []
     for page in document:
-        text = page.get_text()
-        pages_text.append(text)
+        blocks = page.get_text("blocks")
+        blocks.sort(key=lambda block: (block[1], block[0]))
+
+        page_lines = []
+
+        for block in blocks:
+            text = block[4].strip()
+            if text:
+                page_lines.append(text)
+
+        pages_text.append("\n".join(page_lines))
 
     document.close()
 
