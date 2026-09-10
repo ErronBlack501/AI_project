1aa9453d7f1163958eae920395a280bcbc8e3f3d
AndriFitia <andri.fitiavana126@gmail.com>
2026-09-10 15:22:55 +0300

    main.py : configuration de main.py pour cleaner.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
 from app.extraction.pdf_extractor import extract_text_pdf
+from app.preprocessing.cleaner import clean_text
 
 PDF_PATH = "data/test/cv_test.pdf"
 
 def main():
-    text = extract_text_pdf(PDF_PATH)
-
+    # Extraction
+    raw_text = extract_text_pdf(PDF_PATH)
     print("=" * 50)
     print("Texte extrait du CV :")
     print("=" * 50)
-    print(text)
+    print(raw_text)
+
+    # Nettoyage
+    cleaned_text = clean_text(raw_text)
+    print("\n")
+    print("=" * 50)
+    print("Texte nettoyé du CV :")
+    print("=" * 50)
+    print(cleaned_text)
 
 if __name__ == "__main__":
     main()
